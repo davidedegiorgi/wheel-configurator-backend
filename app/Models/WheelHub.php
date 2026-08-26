a6be6105cd17a30411235d566f99a3663250d2a2
davidedegiorgi <degiorgi.davide04@gmail.com>
2026-08-26 13:03:40 +0200

    Apply DT Swiss price overrides

--- app/Models/WheelHub.php
+++ app/Models/WheelHub.php
@@ -12,6 +12,18 @@ class WheelHub extends Model
 
     protected $fillable = ['wheel_category_id', 'name', 'engine_type', 'horsepower', 'price', 'image_url'];
 
+    private const PRICE_OVERRIDES = [
+        'Mozzo DT Swiss 350' => 300.00,
+        'Mozzo DT Swiss 240' => 450.00,
+    ];
+
+    public function getPriceAttribute($value): string
+    {
+        $price = self::PRICE_OVERRIDES[$this->attributes['name'] ?? ''] ?? (float) $value;
+
+        return number_format((float) $price, 2, '.', '');
+    }
+
     public function wheelCategory(): BelongsTo
     {
         return $this->belongsTo(WheelCategory::class, 'wheel_category_id');
